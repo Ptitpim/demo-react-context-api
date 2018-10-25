e89eea5a7e7ac6905b8682f84cc7bc59b6aa7ee4
François POUTHIER <francois.pouthier@gmail.com>
2018-10-25 11:23:28 +0200

    add demo files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Family from './components/Family';
+
+// first we will make a new context
+export const MyContext = React.createContext();
+
+// then create a provider component
+class MyProvider extends Component {
+  state = {
+    name: 'Wes',
+    age: 100,
+    cool: true
+  }
+
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({
+          age: this.state.age + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MyProvider>
+        <div>
+          <p>I'm the app</p>
+          <Family />
+        </div>
+      </MyProvider>
     );
   }
 }
---
+++ src/components/Person.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { MyContext } from '../App';
+
+export default class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>🍰🍥🎂</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
